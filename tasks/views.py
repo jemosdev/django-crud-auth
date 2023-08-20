6b286c4e7f9f5912854263be0a101cb18a2b8519
jemosdev <jerson.mosquera.p@gmail.com>
2023-08-20 15:17:10 -0500

    creating form and register users

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-
 # Create your views here.
-def hello(request):
-    return HttpResponse('mi primer Hello world')
+def index(request):
+    return render(request, 'index.html')
+
+
+def signup(request):
+    
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user= User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+        return HttpResponse('Password do not match')
+
+
