714b414436e716e06c4cd15db3409721f3199f09
jemosdev <jerson.mosquera.p@gmail.com>
2023-08-20 15:31:22 -0500

    handling errors in form of view

--- tasks/views.py
+++ tasks/views.py
@@ -3,23 +3,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
 
 
 def signup(request):
-    
+
     if request.method == 'GET':
         return render(request, 'signup.html', {'form': UserCreationForm
-    })
+                                                })
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user= User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 return HttpResponse('User created successfully')
             except:
-                return HttpResponse('Username already exists')
-        return HttpResponse('Password do not match')
-
-
+                return render(request, 'signup.html', {'form': UserCreationForm, 'error': 'Username already exists'
+                                                        })
+        return render(request, 'signup.html', {'form': UserCreationForm, 'error': 'Password do not match'
+                                                        })
