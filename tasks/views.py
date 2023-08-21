7c19f79ee101bad887cc2ac98d1265e349dd4cf9
jemosdev <jerson.mosquera.p@gmail.com>
2023-08-21 10:24:39 -0500

    complete and delete tasks

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 
 # Create your views here.
 
@@ -36,6 +37,7 @@ def signup(request):
 def tasks(request):
     tasks= Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {'tasks':tasks})
+
 
 def create_task(request):
     
@@ -69,6 +71,20 @@ def task_detail(request,task_id):
         except ValueError:
             return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task'})
 
+
+def completed_task(request, task_id):
+    task= get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+
+def delete_task(request, task_id):
+    task= get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
 
 
 def signout(request):
